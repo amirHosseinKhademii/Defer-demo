08ea2e51fd68b8a360b3d695283dd1549d7a69f0
amirHosseinKhademii <sourena2575@gmail.com>
2022-06-05 11:28:11 +0200

    Add nullable to nullable props

--- Models/Book.cs
+++ Models/Book.cs
@@ -3,13 +3,14 @@ namespace Graph_Demo.Models
     public sealed record Book
     {
         public Guid Id { get; init; }
-        public string Title { get; init; }
 
-        public Author Author { get; init; }
+        public string? Title { get; init; }
+
+        public Author? Author { get; init; }
     }
 
     public sealed record Author
     {
-        public string Name { get; init; }
+        public string? Name { get; init; }
     }
 }
--- Models/Card.cs
+++ Models/Card.cs
@@ -4,37 +4,37 @@ namespace Graph_Demo.Models
     {
         public Guid Id { get; init; }
 
-        // public Header Header { get; init; }
+        public Header? Header { get; init; }
 
-        // public Body Body { get; init; }
+        public Body? Body { get; init; }
 
     }
 
     public sealed record Header
     {
-        public string Title { get; init; }
+        public string? Title { get; init; }
 
         public int Followers { get; init; }
 
-        public string Image { get; init; }
+        public string? Image { get; init; }
 
 
     }
 
     public sealed record Body
     {
-        public string Description { get; init; }
+        public string? Description { get; init; }
 
-        public List<Todo> Todos { get; init; }
+        public List<Todo>? Todos { get; init; }
 
     }
 
     public sealed record Todo
     {
         public Guid Id { get; init; }
-        public string Title { get; init; }
+        public string? Title { get; init; }
 
-        public string Description { get; init; }
+        public string? Description { get; init; }
 
         public bool Completed { get; init; }
 
